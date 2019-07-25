57e0a9bee64d520f5c3c557be688306dbe4ec12f
Stephen Brewer <stephebrewer16@gmail.com>
2019-07-25 17:50:41 -0400

    initial front end skeleton

---
+++ src/containers/MainContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class MainContainer extends Component {
+    componentDidMount = () => {
+        fetch('')
+    }
+    render() {
+        return (
+            <div>
+                {"hey whats up hello"}
+            </div>
+        )
+    }
+}
